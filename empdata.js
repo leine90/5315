7b4f99ce2e97643ca270b01df7f3a5a48cf118c3
eric lein <leine@seattleu.edu>
2018-10-17 01:53:37 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -5,120 +5,120 @@
       "Position": "QuarterBack",
       "salary": "127.3",
       "joining_date": "2011",
-      "office": "Minnesota",
-      "extension": "8"
+      "Team": "Minnesota",
+      "Number": "8"
     },
     {
       "name": "Deandre Hopkins",
       "Position": "Wide Receiver",
       "salary": "125",
       "joining_date": "2013",
-      "office": "Houston",
-      "extension": "10"
+      "Team": "Houston",
+      "Number": "10"
     },
     {
       "name": "Melvin Gordon",
       "Position": "Running Back",
       "salary": "162.5",
       "joining_date": "2015",
-      "office": "San Diego",
-      "extension": "28"
+      "Team": "San Diego",
+      "Number": "28"
     },
     {
       "name": "Adam Thielen",
       "Position": "Wide Receiver",
       "salary": "153.2",
       "joining_date": "2013",
-      "office": "Minnesota",
-      "extension": "19"
+      "Team": "Minnesota",
+      "Number": "19"
     },
     {
       "name": "Sony Michel",
       "Position": "Running Back",
       "salary": "68.8",
       "joining_date": "2018",
-      "office": "New England",
-      "extension": "26"
+      "Team": "New England",
+      "Number": "26"
     },
     {
       "name": "Robert Woods",
       "Position": "Wide Receiver",
       "salary": "114.8",
       "joining_date": "2013",
-      "office": "Los Angeles",
-      "extension": "17"
+      "Team": "Los Angeles",
+      "Number": "17"
     },
     {
       "name": "Justin Tucker",
       "Position": "Kicker",
       "salary": "59",
       "joining_date": "2012",
-      "office": "Baltimore",
-      "extension": "9"
+      "Team": "Baltimore",
+      "Number": "9"
     },
     {
       "name": "Carson Wentz",
       "Position": "QuarterBack",
       "salary": "77.5",
       "joining_date": "2016",
-      "office": "Philadelphia",
-      "extension": "11"
+      "Team": "Philadelphia",
+      "Number": "11"
     },
     {
       "name": "Aaron Jones",
       "Position": "Running Back",
       "salary": "32.9",
       "joining_date": "2017",
-      "office": "Green Bay",
-      "extension": "33"
+      "Team": "Green Bay",
+      "Number": "33"
     },
     {
       "name": "Jimmy Graham",
       "Position": "Tight End",
       "salary": "77.5",
       "joining_date": "2010",
-      "office": "Green Bay",
-      "extension": "80"
+      "Team": "Green Bay",
+      "Number": "80"
     },
     {
       "name": "Calvin Ridley",
       "Position": "Wide Receiver",
       "salary": "94.2",
       "joining_date": "2018",
-      "office": "Atlanta",
-      "extension": "18"
+      "Team": "Atlanta",
+      "Number": "18"
     },
     {
       "name": "Nyheim Hines",
       "Position": "Running",
       "salary": "78.8",
       "joining_date": "2018",
-      "office": "Indianapolis",
-      "extension": "21"
+      "Team": "Indianapolis",
+      "Number": "21"
     },
     {
       "name": "Golden Tate",
       "Position": "Wide Receiver",
       "salary": "95",
       "joining_date": "2010",
-      "office": "Detroit",
-      "extension": "15"
+      "Team": "Detroit",
+      "Number": "15"
     },
     {
       "name": "Aaron Rodgers",
       "Position": "QuarterBack",
       "salary": "133",
       "joining_date": "2005",
-      "office": "Green Bay",
-      "extension": "12"
+      "Team": "Green Bay",
+      "Number": "12"
     },
     {
       "name": "Devante Adams",
       "Position": "Wide Receiver",
       "salary": "138.7",
       "joining_date": "2013",
-      "office": "Green Bay",
-      "extension": "17"
+      "Team": "Green Bay",
+      "Number": "17"
     }
   ]
 }
